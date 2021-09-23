ae5181cbeeaf9fac99793ad36a702bedfc378c73
Nabe Al Hasnawi <n.al-hasnawi@dkfz-heidelberg.de>
2021-09-23 16:45:51 +0200

    added Patientdata in IdCard

--- src/app/model/patient.ts
+++ src/app/model/patient.ts
@@ -1,6 +1,17 @@
+import {FieldService} from "../services/field.service";
+
 export class Patient {
+
   constructor(
-    public fields: {[key: string]: string} = {},
-    public ids: Array<{idType: string, idString: string}> = []
-  ) {}
+    public fields: { [key: string]: any } = {},
+    public ids: Array<{ idType: string, idString: string }> = []
+  ) {
+    let fieldService = new FieldService()
+    fieldService.fields.forEach(value => {
+      if (fields[value.name] === undefined) {
+        fields[value.name]=""
+      }
+    })
+  }
+
 }
