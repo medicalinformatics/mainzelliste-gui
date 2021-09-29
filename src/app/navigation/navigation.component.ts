c5feeecd1187933ab1fff6ca49228e45593b0891
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2021-09-29 16:34:24 +0200

    Sidenavigation Event

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -7,20 +7,19 @@ import { Component, OnInit, Renderer2, RendererFactory2 } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
   private renderer: Renderer2
+  private element: any
   constructor(rendererFactory: RendererFactory2) {this.renderer = rendererFactory.createRenderer(null, null) }
 
   ngOnInit(): void {
   }
-  chBackground(event: any, className: string):void {
-    const hasClass = event.target.parent.classList.contains('active');
-console.log(hasClass)
-    if(hasClass) {
-      this.renderer.removeClass(event.target, 'active');
-    } else {
-      this.renderer.addClass(event.target, 'active');
+  chBackground(event: any):void {
+    if(this.element) {
+      this.renderer.removeClass(this.element, 'active');
     }
-    // document.navigation.nav_item.style.background = '#dddddd'
-}
+    this.renderer.addClass(event.target.parentNode, 'active');
+
+    this.element = event.target.parentNode
+  }
 
 }
 
