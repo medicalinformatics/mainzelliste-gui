2e1c7da656f91d3ce48576cf55ab99afc4182da8
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2021-11-08 16:07:34 +0100

    refactor: Resorted Entries in App Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import {NgModule} from '@angular/core';
-import{ Routes, RouterModule} from '@angular/router';
+import {Routes, RouterModule} from '@angular/router';
 import {PatientlistComponent} from "./patientlist/patientlist.component";
 import {HomeComponent} from "./home/home.component";
 import {AudittrailComponent} from "./audittrail/audittrail.component";
@@ -12,18 +12,16 @@ import {EditPatientComponent} from "./edit-patient/edit-patient.component";
 import {DeletePatientComponent} from "./delete-patient/delete-patient.component";
 import {DeleteMultiplePatientsComponent} from "./delete-multiple-patients/delete-multiple-patients.component";
 
-
 const routes: Routes = [
-  {path: 'patientlist', component: PatientlistComponent},
-  {path: 'create-patient', component: CreatePatientComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'patientlist-view', component: PatientlistViewComponent},
+  {path: 'create-patient', component: CreatePatientComponent},
   {path: 'audittrail', component: AudittrailComponent},
   {path: 'idcard', component: IdcardComponent},
-  {path: 'similar-patient', component: SimilarPatientComponent},
-  {path: 'merge-patients', component: MergePatientsComponent},
-  {path:'patientlist-view', component: PatientlistViewComponent},
   {path: 'edit-patient', component: EditPatientComponent},
   {path: 'delete-patient', component: DeletePatientComponent},
+  {path: 'similar-patient', component: SimilarPatientComponent},
+  {path: 'merge-patients', component: MergePatientsComponent},
   {path: 'delete-multiple-patients', component:DeleteMultiplePatientsComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
