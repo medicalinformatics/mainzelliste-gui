48e72254f667a535bf757c67cbcd13110c1f2efe
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2021-11-08 15:56:06 +0100

    fixed: Delete Multiple Patients now works as expected

--- src/app/delete-multiple-patients/delete-multiple-patients.component.ts
+++ src/app/delete-multiple-patients/delete-multiple-patients.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Patient} from "../model/patient";
 import {PatientService} from "../services/patient.service";
+import {PatientListService} from "../services/patient-list.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-multiple-delete-patients',
@@ -11,7 +13,9 @@ export class DeleteMultiplePatientsComponent implements OnInit {
   patientService: PatientService;
   patients: Array<Patient> = [];
 
-  constructor(patientService: PatientService) {
+  constructor(patientService: PatientService,
+              private patientListService: PatientListService,
+              private router: Router) {
     this.patientService = patientService;
   }
 
@@ -20,12 +24,11 @@ export class DeleteMultiplePatientsComponent implements OnInit {
   }
 
   deletePatientenZeile(){
-    console.log("deleted..");
     this.patients.forEach((patient) => {
-      this.patientService.deletePatient(patient).then((result) => {
-        if (result == 204) {
-          // TODO: What should happen here? This component will not survice the deletePatient call ...
-        }
+      this.patientListService.deletePatient(patient).then(success => {
+        this.patientService.rerenderPatients(this.patientListService.getPatients());
+      }, error => {
+        console.log("This is not so good")
       });
     })
   }
