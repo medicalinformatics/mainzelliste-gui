f8ff273c8670d13426b928560a0afee7e9737f0d
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2021-11-09 13:40:10 +0100

    fix: Corrected Path to Config Template in Entrypoint Script

--- mainzelliste-gui.entrypoint.sh
+++ mainzelliste-gui.entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-envsubst < /usr/share/nginx/html/assets/config.template.json > /usr/share/nginx/html/assets/config.json
+envsubst < /usr/share/nginx/html/assets/config/config.template.json > /usr/share/nginx/html/assets/config/config.json
 bash /docker-entrypoint.sh
