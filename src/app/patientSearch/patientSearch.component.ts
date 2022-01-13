733f8e378520fd0c88e141f9fcc8ca38493061bf
Nabe Al Hasnawi <n.al-hasnawi@dkfz-heidelberg.de>
2022-01-13 14:52:22 +0100

    changed html-Tags of Search Patient and Mat-Checkbox in patientSearch.component.html

--- src/app/patientSearch/patientSearch.component.ts
+++ src/app/patientSearch/patientSearch.component.ts
@@ -3,45 +3,36 @@ import {MatDialogConfig, MatDialogRef} from "@angular/material/dialog";
 import {FormControl, FormGroup} from "@angular/forms";
 import {PatientService} from "../services/patient.service";
 import {Patient} from "../model/patient";
-import {add} from "ionicons/icons";
 
 @Component({
   selector: 'app-patient-search',
   templateUrl: './patientSearch.component.html',
   styleUrls: ['./patientSearch.component.css']
 })
-export class PatientSearchComponent implements OnInit {
 
+export class PatientSearchComponent implements OnInit {
   patientService: PatientService;
   patients: Array<Patient> = [];
-
   @Input() patient: Patient = new Patient()
-  @Input() fields : Array<string> = [];
+  @Input() fields: Array<string> = [];
 
-//Test
-  selectedFilter: Array<string>=[];
-
-  filterOptions: Array<string>=["gleich", "nicht gleich", "Beginnt mit", "Endet mit", "Enthält", "Enthält nicht"];
-  title="Filter";
+  filterOptions: Array<string> = ["gleich", "nicht gleich", "Beginnt mit", "Endet mit", "Enthält", "Enthält nicht"];
+  title = "Filter";
   range = new FormGroup({
     start: new FormControl(),
     end: new FormControl(),
   });
 
-
-  constructor(public dialogRef: MatDialogRef<PatientSearchComponent>,patientService: PatientService) {
+  constructor(public dialogRef: MatDialogRef<PatientSearchComponent>, patientService: PatientService) {
     this.patientService = patientService;
-
   }
+
   ngOnInit(): void {
-
   }
 
-  closeFilter(){
+  closeFilter() {
     console.log("closedFilter");
     this.dialogRef.close();
   }
 
-  updatePatientlist(){
-  }
 }
