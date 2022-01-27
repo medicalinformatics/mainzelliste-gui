1e941f6e4c1d24a1bd9f86c4a4e4023e658b2dac
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2022-01-27 11:23:20 +0100

    fix: Mirrored Component renaming to their Test Specification

--- src/app/navigation/navigation.component.spec.ts
+++ src/app/navigation/navigation.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavigationsseiteComponent } from './navigation.component';
+import { NavigationComponent } from './navigation.component';
 
 describe('NavigationsseiteComponent', () => {
-  let component: NavigationsseiteComponent;
-  let fixture: ComponentFixture<NavigationsseiteComponent>;
+  let component: NavigationComponent;
+  let fixture: ComponentFixture<NavigationComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ NavigationsseiteComponent ]
+      declarations: [ NavigationComponent]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavigationsseiteComponent);
+    fixture = TestBed.createComponent(NavigationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/similarPatient/similarPatient.component.spec.ts
+++ src/app/similarPatient/similarPatient.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CloneComponent } from './similarPatient.component';
+import { SimilarPatientComponent } from './similarPatient.component';
 
 describe('CloneComponent', () => {
-  let component: CloneComponent;
-  let fixture: ComponentFixture<CloneComponent>;
+  let component: SimilarPatientComponent;
+  let fixture: ComponentFixture<SimilarPatientComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CloneComponent ]
+      declarations: [ SimilarPatientComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CloneComponent);
+    fixture = TestBed.createComponent(SimilarPatientComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
