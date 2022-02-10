4d319c86d8ce87b39c22cbcb57f0a58b07b51dec
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2022-02-10 10:41:51 +0100

    wip

--- mainzelliste-gui.entrypoint.sh
+++ mainzelliste-gui.entrypoint.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 envsubst < /usr/share/nginx/html/assets/config/config.template.json > /usr/share/nginx/html/assets/config/config.json
+# Deployment context muss laut angular docu form /my/app/ haben
+if [ -n "$NGINX_DEPLOYMENT_CONTEXT" ]; then
+  echo "Adjusting apps base href to: ${NGINX_DEPLOYMENT_CONTEXT}";
+  sed -i -e 's|<base href="/">|<base href='/"$NGINX_DEPLOYMENT_CONTEXT"/'>|' index.html;
+fi
