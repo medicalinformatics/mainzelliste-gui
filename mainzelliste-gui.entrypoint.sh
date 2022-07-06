830a8b965e6213efa12e48deb95fe7f7cf0b7fd6
Torben Brenner <t.brenner@dkfz-heidelberg.de>
2022-07-06 12:00:37 +0200

    feature: Added Secrets Support to Entrypoint

--- mainzelliste-gui.entrypoint.sh
+++ mainzelliste-gui.entrypoint.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
+if [ -d "/run/secrets" ]; then
+  for file in "/run/secrets/"*; do
+    echo "Found $file in /run/secrets";
+    if test -f "$file"; then
+      envName=$(echo "$file" | tr '[:lower:]' '[:upper:]' | sed -e "s|/RUN/SECRETS/||" -e "s|_SECRET||");
+      echo "Exporting environment for $envName from $file";
+      eval export "$envName"="$(cat $file)";
+    fi
+  done
+fi
 envsubst < /usr/share/nginx/html/assets/config/config.template.json > /usr/share/nginx/html/assets/config/config.json
