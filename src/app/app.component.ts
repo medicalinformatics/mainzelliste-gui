4c14b5d740462d9447e410e5798b32904e5a4995
Moanes Ben Amor <m.benamor@dkfz-heidelberg.de>
2023-03-27 09:56:56 +0200

    add: show loading page before the authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {SessionService} from "./services/session.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mainzelliste-gui';
+
+  constructor(
+    public readonly sessionService: SessionService
+  ) {
+  }
 }
