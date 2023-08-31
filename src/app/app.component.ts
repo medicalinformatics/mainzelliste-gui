92ba6a100ada1ccc59de4864d601c8715342bc7b
Moanes Ben Amor <m.benamor@dkfz-heidelberg.de>
2023-08-31 18:13:12 +0200

    fix: clean error messages, when a new pages loaded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import {Component} from '@angular/core';
 import {SessionService} from "./services/session.service";
 import {GlobalTitleService} from "./services/global-title.service";
+import {ErrorNotificationService} from "./services/error-notification.service";
+import {NavigationStart, Router} from "@angular/router";
+import {Observable} from "rxjs";
+import {filter} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +16,14 @@ export class AppComponent {
 
   constructor(
     public readonly sessionService: SessionService,
-    public readonly titleService: GlobalTitleService
+    public readonly titleService: GlobalTitleService,
+    public readonly errorNotificationService: ErrorNotificationService,
+    public router: Router
   ) {
+    (router.events.pipe(
+      filter(evt => evt instanceof NavigationStart)
+    ) as Observable<NavigationStart>).subscribe(() =>
+      this.errorNotificationService.clearMessages()
+    );
   }
 }
