f2a7e2335067d35136523f57bed4b2ffab364028
Moanes Ben Amor <m.benamor@dkfz-heidelberg.de>
2023-09-17 23:07:27 +0200

    fix: compilation errors

--- src/app/shared/login-again/login-again.dialog.spec.ts
+++ src/app/shared/login-again/login-again.dialog.spec.ts
@@ -1,20 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {LoginAgainDialog} from "./login-again.dialog";
 
-import { PatientSearchComponent } from './patientSearch.component';
-
-describe('PatientSearchComponent', () => {
-  let component: PatientSearchComponent;
-  let fixture: ComponentFixture<PatientSearchComponent>;
+describe('LoginAgainComponent', () => {
+  let component: LoginAgainDialog;
+  let fixture: ComponentFixture<LoginAgainDialog>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PatientSearchComponent ]
+      declarations: [ LoginAgainDialog ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PatientSearchComponent);
+    fixture = TestBed.createComponent(LoginAgainDialog);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
