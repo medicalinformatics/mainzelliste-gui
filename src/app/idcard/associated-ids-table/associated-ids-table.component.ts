81ceec227610a8c0f7dca3fee0cbd045a410870f
Apocalypse-jk <kunz_jonas@outlook.de>
2024-05-30 14:40:30 +0200

    added idType information in Table header; added index column to table

--- src/app/idcard/associated-ids-table/associated-ids-table.component.ts
+++ src/app/idcard/associated-ids-table/associated-ids-table.component.ts
@@ -12,6 +12,7 @@ export class AssociatedIdsTableComponent implements OnInit {
   elementData: Element[] = [];
   displayedColumns: string[] = [];
   dataSource!: MatTableDataSource<Element>;
+  headers: {name: string, id: string, intOrExt: string}[] = [{name: "#", id: "index", intOrExt: ""}];
 
   @Input() data!: {idTypes: {name: string, id: string, isExternal: boolean}[] | undefined, group: AssociatedIdGroup};
 
@@ -23,27 +24,35 @@ export class AssociatedIdsTableComponent implements OnInit {
   ngOnInit(): void {
     this.fillElementData();
     this.dataSource = new MatTableDataSource(this.elementData);
+    this.setupIdTypes();
   }
 
   fillElementData() {
     if (this.data != undefined && this.data.idTypes != undefined) {
-      this.displayedColumns = this.data.idTypes.map(id => id.id);
-      this.data.group.associatedIds.forEach(associatedId => {
-        let tempIds: string[] = [];
+      this.displayedColumns = ["index"].concat(this.data.idTypes.map(id => id.id));
+      for (let y = 0; y < this.data.group.associatedIds.length; y++) {
+        let associatedId = this.data.group.associatedIds[y];
+        let tempIds: string[] = [(y + 1).toString()];
         for (let x = 0; x < associatedId.idTypes.length; x++) {
           for (let i = 0; i < this.data.idTypes!.length; i++) {
             if (associatedId.idTypes[x].idType === this.data.idTypes![i].id) {
-              tempIds[i] = associatedId.idTypes[x].idString;
+              tempIds[i+1] = associatedId.idTypes[x].idString;
             }
           }
         }
         this.elementData.push({ids: tempIds});
-      });
-    } else {
-      
+      }
     }
   }
 
+  setupIdTypes() {
+    if (this.data != undefined && this.data.idTypes != undefined) {
+      this.data.idTypes.forEach(idType => {
+        let temp = idType.isExternal ? "(ext)" : "(int)";
+        this.headers.push({name: idType.name, id: idType.id, intOrExt: temp});
+      });
+    }
+  }
 }
 
 export interface Element {
