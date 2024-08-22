6b560ca86dff1f5cd89272a73f27645b52829e18
woerterreise <56866655+paolaklein@users.noreply.github.com>
2024-08-22 16:22:22 +0200

    feat: choose Main Patient + PopUp to confirm

--- src/app/experimental/mergePatients/mergePatients.component.ts
+++ src/app/experimental/mergePatients/mergePatients.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
 import { Patient } from "../../model/patient";
 import { PatientService } from "../../services/patient.service";
 import { GlobalTitleService } from "../../services/global-title.service";
@@ -6,9 +6,11 @@ import { TranslateService } from "@ngx-translate/core";
 import { PatientListService } from "../../services/patient-list.service";
 import { Id } from "../../model/id";
 import { HttpErrorResponse } from "@angular/common/http";
-import { ActivatedRoute, Router } from "@angular/router";
+import { Router } from "@angular/router";
 import { MainzellisteUnknownError } from "../../model/mainzelliste-unknown-error";
 import { throwError } from "rxjs";
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
+import type { DialogProps } from "@mui/material";
 
 @Component({
   selector: 'app-merge-patients',
@@ -17,40 +19,28 @@ import { throwError } from "rxjs";
 })
 export class MergePatientsComponent implements OnInit {
   patientService: PatientService;
- // @Input() patients: Array<Patient> = [];
+  // @Input() patients: Array<Patient> = [];
   @Input() patient1: Patient = new Patient;
   @Input() patient2: Patient = new Patient;
 
   @Input() fields: Array<string> = [];
-
-  @Input() finalPatient = new Patient;
-  emptyPatient = new Patient;
   mainPatient: number = 1; // Index of the patient that is the main patient (aka which ID is the main ID)
-
+  confirm: boolean = false;
   constructor(
     public translate: TranslateService,
     patientService: PatientService,
     private titleService: GlobalTitleService,
     private patientListService: PatientListService,
-    private activatedRoute: ActivatedRoute,
+    public confirmDialog: MatDialog,
     private router: Router,
   ) {
     this.patientService = patientService;
-    this.emptyPatient.ids = [{ idType: "Pseudonym", idString: " ", tentative: false }];
-    this.titleService.setTitle(this.translate.instant('mergePatients.title'), false, "merge_type");
-    this.activatedRoute.params.subscribe((params) => {
-      console.log(params);
-
-    });
+    this.titleService.setTitle(this.translate.instant('mergePatients.title'), false, "merge_type")
   }
 
   ngOnInit(): void {
     this.loadPatient();
     //  this.patients = history.state.patients;
-  }
-
-  updateFieldsOfFinalPatient(name: string, value: string) {
-    this.finalPatient.fields[name] = value;
   }
 
 
@@ -85,13 +75,25 @@ export class MergePatientsComponent implements OnInit {
       });
   }
 
-  updatePseudonymOfFinalPatient(name: string, value: string) {
-    // this.finalPatient.ids[name]=value;
-    this.finalPatient.ids = this.mainPatient == 1 ? this.patient1.ids : this.patient2.ids;
-  }
+  async mergePatients() {
+    // TODO: Popup to confirm and choose if delete other or keep then route to id card of final patient
+    let deleteFlag: boolean = false;
+    const dialogRef = this.confirmDialog.open(MergePatientConfirmDialog, {
+      width: '900px',
+      data: { mainPatient: this.mainPatient, isDelete: deleteFlag }
+    });
 
-  mergePatients() {
-    // TODO: Needs Backend Implementation
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.confirm = result.confirm ?? false;
+        deleteFlag = result.isDelete;
+
+        if (this.confirm) {
+          let finalPatient = this.mainPatient == 1 ? this.patient1 : this.patient2;
+          this.router.navigate(['/idcard', finalPatient.ids[0].idType, finalPatient.ids[0].idString]);
+        }
+      }
+    });
   }
 
   setMainPatient(index: number) {
@@ -99,5 +101,29 @@ export class MergePatientsComponent implements OnInit {
   }
   getMainPatient() {
     return this.mainPatient;
+  }
+
+}
+
+@Component({
+  selector: 'mergePatient-confirm-dialog',
+  templateUrl: 'mergePatient-confirm-dialog.html',
+})
+export class MergePatientConfirmDialog {
+  constructor(
+    public dialogRef: MatDialogRef<MergePatientConfirmDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: { mainPatient: number, isDelete: boolean }
+  ) {
+  }
+
+  cancel(isDelete: boolean): void {
+    const handleClose: DialogProps["onClose"] = (event: any, reason: string) => {
+      if (reason && reason === "backdropClick") {
+        this.dialogRef.close({ isDelete: isDelete, confirm: false });
+        return;
+      }
+    }
+    this.dialogRef.close({ isDelete: isDelete, confirm: true });
+
   }
 }
