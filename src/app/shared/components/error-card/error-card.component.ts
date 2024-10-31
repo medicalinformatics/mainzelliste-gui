1f0a8712d88f5cc6dafa46059c8c1f79a7e97f46
Moanes Ben Amor <m.benamor@dkfz-heidelberg.de>
2024-10-31 14:06:45 +0100

    refactor: control content page margin left in right in configuration.component.html, dynamically change the width of error message and fix the height of pseudonyms and patient data in IDCard page

--- src/app/shared/components/error-card/error-card.component.ts
+++ src/app/shared/components/error-card/error-card.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-error-card',
@@ -9,11 +10,22 @@ export class ErrorCardComponent implements OnInit {
 
   @Input() messages: string[] = [];
   @Output() messagesChange = new EventEmitter<string[]>();
+  widthPageMap: Map<string,string> = new Map([
+    ["add-new-patient", "41%"],
+    ["edit-patient", "92%"],
+    ["idcard", "94%"],
+  ]);
 
-  constructor() {
+  constructor(
+    private router: Router) {
   }
 
   ngOnInit(): void {
+  }
+
+  public getWidth(): string {
+    let urlSegment = this.router.url.split('/');
+    return urlSegment.length < 2 ? "100%" : this.widthPageMap.get(urlSegment[1]) ?? "100%";
   }
 
   closeError(message: string) {
