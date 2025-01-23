23f77b99e51d633d67e26f5ca2e3fe4fc94943d3
Klein <paola.klein@dkfz-heidelberg.de>
2025-01-23 13:54:31 +0100

    Fix: error in Readme& PatientList : GeburtSdatum

--- src/app/model/patientlist.ts
+++ src/app/model/patientlist.ts
@@ -11,7 +11,7 @@ export class PatientList {
       new Field("first_name_text", "Vorname", "vorname", [], SemanticType.FIRSTNAME, FieldType.TEXT, true, "", "Max"),
       new Field("last_name_text", "Nachname", "nachname", [], SemanticType.LASTNAME, FieldType.TEXT, true, "", "Mustermann"),
       new Field("birth_name_text", "Geburtsname", "geburtsname", [], SemanticType.BIRTH_NAME , FieldType.TEXT, true, "", "falls vorhanden"),
-      new Field("birth_date_text", "Geburtdatum", "", ["geburtstag", "geburtsmonat", "geburtsjahr"], SemanticType.UNDEFINED , FieldType.DATE, true, "", "00.00.0000"),
+      new Field("birth_date_text", "Geburtsdatum", "", ["geburtstag", "geburtsmonat", "geburtsjahr"], SemanticType.UNDEFINED , FieldType.DATE, true, "", "00.00.0000"),
       new Field("residence_text", "Wohnort", "ort", [], SemanticType.CITY, FieldType.TEXT, true, "", "Musterstadt"),
       new Field("zip_code_text", "PLZ", "plz", [], SemanticType.POSTAL_CODE, FieldType.TEXT, true, "", "mind. 5 Zeichen")
     ],
